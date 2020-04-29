115bfeaeff4f9f595b3b0b69bf6ad711d7604ea1
Ankit Gohel <ankitgohel@asana.com>
2020-04-28 19:57:44 -0700

    Filter enabled custom fields

---
+++ test/asana/helpers/test_get_custom_field_enum_option_id.py
@@ -0,0 +1,57 @@
+from src.asana import helpers as asana_helpers
+
+from test.impl.base_test_case_class import BaseClass
+from test.impl.builders.helpers import get_custom_field_settings_for_test
+
+
+class TestGetCustomFieldEnumOptionId(BaseClass):
+    @classmethod
+    def setUpClass(cls):
+        cls.custom_field_gid = "11111"
+        cls.custom_field_name = "Test field"
+        cls.enabled_enum_option_gid = "22222"
+        cls.enabled_enum_option_name = "enabled"
+        cls.disabled_enum_option_gid = "33333"
+        cls.disabled_enum_option_name = "disabled"
+        cls.custom_field_data = get_custom_field_settings_for_test(
+            custom_field_gid=cls.custom_field_gid,
+            custom_field_name=cls.custom_field_name,
+            enabled_enum_option_gid=cls.enabled_enum_option_gid,
+            enabled_enum_option_name=cls.enabled_enum_option_name,
+            disabled_enum_option_gid=cls.disabled_enum_option_gid,
+            disabled_enum_option_name=cls.enabled_enum_option_name,
+        )
+
+    def test_get_valid_custom_field_enum_option_id(self):
+        enum_option_id = asana_helpers._get_custom_field_enum_option_id(
+            self.custom_field_name,
+            self.enabled_enum_option_name,
+            self.custom_field_data,
+        )
+        self.assertEqual(enum_option_id, self.enabled_enum_option_gid)
+
+    def test_invalid_custom_field(self):
+        enum_option_id = asana_helpers._get_custom_field_enum_option_id(
+            "RandomField", self.enabled_enum_option_name, self.custom_field_data
+        )
+        self.assertIsNone(enum_option_id)
+
+    def test_invalid_enum_option(self):
+        enum_option_id = asana_helpers._get_custom_field_enum_option_id(
+            self.custom_field_name, "RandomEnumOption", self.custom_field_data
+        )
+        self.assertIsNone(enum_option_id)
+
+    def test_get_enum_option_id_for_disabled_option(self):
+        enum_option_id = asana_helpers._get_custom_field_enum_option_id(
+            self.custom_field_name,
+            self.disabled_enum_option_name,
+            self.custom_field_data,
+        )
+        self.assertIsNone(enum_option_id)
+
+
+if __name__ == "__main__":
+    from unittest import main as run_tests
+
+    run_tests()
--- test/impl/builders/helpers.py
+++ test/impl/builders/helpers.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 from datetime import datetime
-from typing import Union
+from typing import Union, List, Dict
 from src.utils import create_date_string
 
 
@@ -12,3 +12,53 @@ def transform_datetime(input_datetime: Union[str, datetime]) -> str:
 
 def create_uuid() -> str:
     return uuid4().hex
+
+
+def get_custom_field_settings_for_test(
+    custom_field_gid: str = "12345",
+    custom_field_name: str = "test",
+    enabled_enum_option_gid: str = "67890",
+    enabled_enum_option_name: str = "enabled option",
+    disabled_enum_option_gid: str = "67890",
+    disabled_enum_option_name: str = "disabled option",
+) -> List[Dict]:
+    return [
+        {
+            "gid": "11111",
+            "custom_field": {
+                "gid": custom_field_gid,
+                "enum_options": [
+                    {
+                        "gid": enabled_enum_option_gid,
+                        "color": "green",
+                        "enabled": True,
+                        "name": enabled_enum_option_name,
+                        "resource_type": "enum_option",
+                    },
+                    {
+                        "gid": disabled_enum_option_gid,
+                        "color": "red",
+                        "enabled": False,
+                        "name": disabled_enum_option_name,
+                        "resource_type": "enum_option",
+                    },
+                ],
+                "name": custom_field_name,
+                "resource_subtype": "enum",
+                "resource_type": "custom_field",
+                "type": "enum",
+            },
+            "is_important": True,
+            "parent": {
+                "gid": "22222",
+                "name": "Test Project",
+                "resource_type": "project",
+            },
+            "project": {
+                "gid": "33333",
+                "name": "Test Project",
+                "resource_type": "project",
+            },
+            "resource_type": "custom_field_setting",
+        }
+    ]
