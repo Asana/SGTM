007200ea4552ceee1c2f8c0de713843f5fbdc4d3
Greg Einfrank <greg.einfrank@gmail.com>
2020-05-13 16:56:23 -0700

    Updates to the dynamodb lock

--- src/dynamodb/lock.py
+++ src/dynamodb/lock.py
@@ -1,3 +1,5 @@
+from typing import Optional
+from datetime import timedelta
 import boto3  # type: ignore
 from contextlib import contextmanager
 from src.config import LOCK_TABLE
@@ -7,12 +9,20 @@ from python_dynamodb_lock.python_dynamodb_lock import DynamoDBLockClient  # type
 dynamodb_resource = boto3.resource("dynamodb")
 
 
-def dynamodb_lock(pull_request_id: str):
+@contextmanager
+def dynamodb_lock(pull_request_id: str, retry_timeout: Optional[timedelta] = None):
     lock_client = DynamoDBLockClient(
         dynamodb_resource,
         table_name=LOCK_TABLE,
         # partition_key_name="key",
         # sort_key_name="key",
     )
+
     # TODO: Make this match get-lock-client in the clojure code
-    return lock_client.acquire_lock(pull_request_id, sort_key=pull_request_id)
+    lock = lock_client.acquire_lock(
+        pull_request_id, sort_key=pull_request_id, retry_timeout=retry_timeout
+    )
+    try:
+        yield lock
+    finally:
+        lock.release(best_effort=True)
