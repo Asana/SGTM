48b8ace3d807ddcdc1a4ac2357f60cbe059775ad
Eric Walton <ericwalton@asana.com>
2020-05-13 16:56:05 -0700

    remove unneeded XCXC

--- src/github/models/pull_request_review_comment.py
+++ src/github/models/pull_request_review_comment.py
@@ -4,6 +4,4 @@ from .review import Review
 class PullRequestReviewComment(Comment):
 
     def review(self) -> Review:
-        # XCXC: Make a separate type for IssueComment and PullRequestReviewComment
-        # only the latter would implement this.
         return Review(self._raw['pullRequestReview'])
