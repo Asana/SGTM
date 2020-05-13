5ccc283df4c58b4dbbd3f6fac993d1e0e0f27408
Eric Walton <ericwalton@asana.com>
2020-05-12 17:46:06 -0700

    Clarify comment.

--- src/asana/helpers.py
+++ src/asana/helpers.py
@@ -319,7 +319,7 @@ def _task_followers_from_pull_request(pull_request: PullRequest):
 def _wrap_in_tag(tag_name: str, attrs: Optional[Dict[str, str]] = None) -> Callable[[str], str]:
 
     if attrs is not None:
-        # This will always start with a blank space, so it's separate from the tag name.
+        # This will always start with a blank space, so that it's separate from the tag name.
         attr_list = "".join(f' {k}="{escape(v)}"' for k, v in attrs.items())
     else:
         attr_list = ""
