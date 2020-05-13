154b7576ebce9981de0bccffaa4d1a398828c2cd
Eric Walton <ericwalton@asana.com>
2020-05-12 17:08:55 -0700

    rename venv used in zip_lambda_code.sh so it is less likely to  match user-created one

--- scripts/zip_lambda_code.sh
+++ scripts/zip_lambda_code.sh
@@ -5,14 +5,14 @@ set -e # Exit immediately if a command exits with a non-zero status.
 # https://docs.aws.amazon.com/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html#python-package-venv
 
 # Create a virtualenvironment and activate
-python3 -m venv v-env
-source v-env/bin/activate
+python3 -m venv build-env
+source build-env/bin/activate
 
 # Install the production Python requirements
 pip3 install -r requirements.txt
 
 # Zip the dependencies into the output archive
-cd v-env/lib/python3.7/site-packages
+cd build-env/lib/python3.7/site-packages
 zip -r9 ${OLDPWD}/build/function.zip .
 
 # Zip the source code into the output archive
