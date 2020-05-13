3a96bbc4a5481f2e83e25c2c72a1cf2b0134d358
Eric Walton <ericwalton@asana.com>
2020-05-12 18:16:33 -0700

    more star wars

--- test/asana/helpers/test_wrap_in_tag.py
+++ test/asana/helpers/test_wrap_in_tag.py
@@ -6,12 +6,12 @@ from test.impl.builders import builder, build
 class TestWrapInTag(BaseClass):
 
     def test_no_attrs_and_empty_text(self):
-        actual = _wrap_in_tag("body")("")
-        self.assertEqual(actual, "<body></body>")
+        actual = _wrap_in_tag("movie")("")
+        self.assertEqual(actual, "<movie></movie>")
 
     def test_basic_usage(self):
-        actual = _wrap_in_tag("h1")("Hi! My name is SGTM!")
-        self.assertEqual(actual, "<h1>Hi! My name is SGTM!</h1>")
+        actual = _wrap_in_tag("movie")("A long time ago in a galaxy far, far away...")
+        self.assertEqual(actual, "<movie>A long time ago in a galaxy far, far away...</movie>")
 
     def test_nested_tags(self):
         actual = _wrap_in_tag("p")("No. " + _wrap_in_tag("strong")("I") + " am your father.")
@@ -22,20 +22,20 @@ class TestWrapInTag(BaseClass):
         self.assertEqual(actual, '<p force="enabled">These aren\'t the droids you\'re looking for.</p>')
 
     def test_multiple_attributes(self):
-        actual = _wrap_in_tag("h1", {"class": "im-a-class", "id": "123"})("Hi! My name is SGTM!")
-        self.assertEqual(actual, '<h1 class="im-a-class" id="123">Hi! My name is SGTM!</h1>')
+        actual = _wrap_in_tag("p", {"by": "leia", "with": "snark"})("Why you stuck-up, half-witted, scruffy-looking nerf herder.")
+        self.assertEqual(actual, '<p by="leia" with="snark">Why you stuck-up, half-witted, scruffy-looking nerf herder.</p>')
 
-    def test_attr_value_that_contains_quote(self):
-        actual = _wrap_in_tag("h1", {"id": '1"23'})("Hi! My name is SGTM!")
-        self.assertEqual(actual, '<h1 id="1&quot;23">Hi! My name is SGTM!</h1>')
+    def test_attr_value_that_contains_special_characters(self):
+        actual = _wrap_in_tag("clone", {"cloned": '<clone></clone>'})("")
+        self.assertEqual(actual, '<clone cloned="&lt;clone&gt;&lt;/clone&gt;"></clone>')
 
     def test_text_contains_emoji(self):
-        actual = _wrap_in_tag("li")("👍")
-        self.assertEqual(actual, '<li>👍</li>')
+        actual = _wrap_in_tag("li")("...the ship that made the Kessel Run in less than twelve parsecs...🚀")
+        self.assertEqual(actual, '<li>...the ship that made the Kessel Run in less than twelve parsecs...🚀</li>')
 
     def test_attribute_value_contains_emoji(self):
-        actual = _wrap_in_tag("li", {'reaction': "👍"})("Happy to see it!")
-        self.assertEqual(actual, '<li reaction="👍">Happy to see it!</li>')
+        actual = _wrap_in_tag("p", {'reaction': "👍👍👍👍👍"})("Five stars! Bravo!")
+        self.assertEqual(actual, '<p reaction="👍👍👍👍👍">Five stars! Bravo!</p>')
 
 
 if __name__ == "__main__":
