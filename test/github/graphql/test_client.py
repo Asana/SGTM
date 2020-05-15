d5afee1ebb059344c56899fea02293417ddd12ab
Eric Walton <ericwalton@asana.com>
2020-05-14 17:15:02 -0700

    Add test for 2nd batch

--- test/github/graphql/test_client.py
+++ test/github/graphql/test_client.py
@@ -31,7 +31,7 @@ class TestGithubClientGetReviewForDatabaseId(BaseClass):
         ])
         self.assertEqual(mock_query.call_count, 2)
 
-    def test_when_one_review_matches__should_return_it(self, mock_query):
+    def test_when_review_in_first_batch_matches__should_return_it(self, mock_query):
         matching_node = {'id': 'matching-review', 'databaseId': self.REVIEW_DB_ID}
         other_node = {'id': 'other-review', 'databaseId': 'doesnt match'}
         mock_query.side_effect = [
@@ -42,7 +42,28 @@ class TestGithubClientGetReviewForDatabaseId(BaseClass):
         actual = client.get_review_for_database_id(self.PULL_REQUEST_ID, self.REVIEW_DB_ID)
 
         self.assertEqual(actual.id(), 'matching-review')
+
+        # Should onlly be called once, since the matching review is in the first batch returned by graphql.
         mock_query.assert_called_once_with("IterateReviews", {"pullRequestId": self.PULL_REQUEST_ID})
+
+    def test_when_review_in_second_batch_matches__should_return_it(self, mock_query):
+        matching_node = {'id': 'matching-review', 'databaseId': self.REVIEW_DB_ID}
+        other_node = {'id': 'other-review', 'databaseId': 'doesnt match'}
+        mock_query.side_effect = [
+            {"node": {"reviews": {"edges": [{'cursor': 'cursor-1', 'node': other_node}]}}},
+            {"node": {"reviews": {"edges": [{'cursor': 'cursor-2', 'node': matching_node}]}}},
+            {"node": {"reviews": {"edges": []}}}
+        ]
+        actual = client.get_review_for_database_id(self.PULL_REQUEST_ID, self.REVIEW_DB_ID)
+
+        self.assertEqual(actual.id(), 'matching-review')
+
+        # Should onlly be called twice, since the matching review is in the first batch returned by graphql.
+        mock_query.assert_has_calls([
+            call("IterateReviews", {"pullRequestId": self.PULL_REQUEST_ID}),
+            call("IterateReviews", {"pullRequestId": self.PULL_REQUEST_ID, "cursor": "cursor-1"})
+        ])
+        self.assertEqual(mock_query.call_count, 2)
 
 
 if __name__ == "__main__":
