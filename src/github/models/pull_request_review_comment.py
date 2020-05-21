97309062445275976bd873ddf33e927530b78a33
Eric Walton <ericwalton@asana.com>
2020-05-21 09:32:53 -0700

    Commented out code

--- src/github/models/pull_request_review_comment.py
+++ src/github/models/pull_request_review_comment.py
@@ -4,6 +4,6 @@ from .comment import Comment
 class PullRequestReviewComment(Comment):
     # def review(self) -> Review:
     #     return Review(self._raw["pullRequestReview"])
-
+    # XCXC remove commented-out code.
     def is_reply(self) -> bool:
         return self._raw["replyTo"] is not None
