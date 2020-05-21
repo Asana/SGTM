bd5c951fd783fa4a7c98cae9affa4f72cc7d9fb9
Eric Walton <ericwalton@asana.com>
2020-05-21 11:30:25 -0700

    Remove XCXC, I think not needed

--- src/github/graphql/fragments/FullComment.py
+++ src/github/graphql/fragments/FullComment.py
@@ -9,8 +9,6 @@
   This is a problem because if A imports B and C; and B and C both import D, then A will be a syntax error.
   So, we store the queries / fragments as sets, formed as a union of itself with all of its dependencies.
   At query time, we convert each set to a single string, with elements separated by new lines.
-
-  XCXC: Consider splitting this fragment into Comment and PullRequestReviewComment.
 """
 
 from typing import FrozenSet
