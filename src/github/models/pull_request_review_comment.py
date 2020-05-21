d75192647c132df87e40d8e21a45b1b049ba740c
Eric Walton <ericwalton@asana.com>
2020-05-21 10:26:18 -0700

    Use future annotations to avoid cyclic import issue

--- src/github/models/pull_request_review_comment.py
+++ src/github/models/pull_request_review_comment.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from .comment import Comment
 from . import review
 
--- src/github/models/review.py
+++ src/github/models/review.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Dict, Any, List
 from datetime import datetime
 from src.utils import parse_date_string
