fe119bf3d6bf3acee092d474ddf1800f9fbe741b
Eric Walton <ericwalton@asana.com>
2020-06-01 17:28:35 -0700

    black

--- test/github/test_webhook.py
+++ test/github/test_webhook.py
@@ -6,25 +6,29 @@ from src.github import webhook
 from src.github.models import PullRequest, PullRequestReviewComment, Review
 
 
-@patch.object(webhook, 'dynamodb_lock')
-@patch('src.github.controller.delete_comment')
-@patch('src.github.controller.upsert_review')
+@patch.object(webhook, "dynamodb_lock")
+@patch("src.github.controller.delete_comment")
+@patch("src.github.controller.upsert_review")
 class TestHandlePullRequestReviewComment(BaseClass):
-
-    @patch.object(Review, 'from_comment')
-    @patch('src.github.graphql.client.get_pull_request_and_comment')
-    def test_comment_edit(self, get_pull_request_and_comment, review_from_comment, upsert_review, delete_comment, lock):
+    @patch.object(Review, "from_comment")
+    @patch("src.github.graphql.client.get_pull_request_and_comment")
+    def test_comment_edit(
+        self,
+        get_pull_request_and_comment,
+        review_from_comment,
+        upsert_review,
+        delete_comment,
+        lock,
+    ):
         payload = {
-            "pull_request": {
-                "node_id": "abcde"
-            },
+            "pull_request": {"node_id": "abcde"},
             "action": "edited",
-            "comment": {
-                "node_id": "hijkl",
-                "pull_request_review_id": "1234566"
-            }
+            "comment": {"node_id": "hijkl", "pull_request_review_id": "1234566"},
         }
-        pull_request, comment = MagicMock(spec=PullRequest), MagicMock(spec=PullRequestReviewComment)
+        pull_request, comment = (
+            MagicMock(spec=PullRequest),
+            MagicMock(spec=PullRequestReviewComment),
+        )
         review = MagicMock(spec=Review)
         get_pull_request_and_comment.return_value = pull_request, comment
         review_from_comment.return_value = review
@@ -36,18 +40,20 @@ class TestHandlePullRequestReviewComment(BaseClass):
         review_from_comment.assert_called_once_with(comment)
         delete_comment.assert_not_called()
 
-    @patch('src.github.graphql.client.get_pull_request')
-    @patch('src.github.graphql.client.get_review_for_database_id')
-    def test_comment_deletion_when_review_still_present(self, get_review_for_database_id, get_pull_request, upsert_review, delete_comment, lock):
+    @patch("src.github.graphql.client.get_pull_request")
+    @patch("src.github.graphql.client.get_review_for_database_id")
+    def test_comment_deletion_when_review_still_present(
+        self,
+        get_review_for_database_id,
+        get_pull_request,
+        upsert_review,
+        delete_comment,
+        lock,
+    ):
         payload = {
-            "pull_request": {
-                "node_id": "abcde"
-            },
+            "pull_request": {"node_id": "abcde"},
             "action": "deleted",
-            "comment": {
-                "node_id": "hijkl",
-                "pull_request_review_id": "1234566"
-            }
+            "comment": {"node_id": "hijkl", "pull_request_review_id": "1234566"},
         }
 
         pull_request = MagicMock(spec=PullRequest)
@@ -62,18 +68,23 @@ class TestHandlePullRequestReviewComment(BaseClass):
         get_review_for_database_id.assert_called_once_with("abcde", "1234566")
         delete_comment.assert_not_called()
 
-    @patch('src.github.graphql.client.get_pull_request', return_value=MagicMock(spec=PullRequest))
-    @patch('src.github.graphql.client.get_review_for_database_id', return_value=None)
-    def test_comment_deletion_when_review_not_found(self, get_review_for_database_id, get_pull_request, upsert_review, delete_comment, lock):
+    @patch(
+        "src.github.graphql.client.get_pull_request",
+        return_value=MagicMock(spec=PullRequest),
+    )
+    @patch("src.github.graphql.client.get_review_for_database_id", return_value=None)
+    def test_comment_deletion_when_review_not_found(
+        self,
+        get_review_for_database_id,
+        get_pull_request,
+        upsert_review,
+        delete_comment,
+        lock,
+    ):
         payload = {
-            "pull_request": {
-                "node_id": "abcde"
-            },
+            "pull_request": {"node_id": "abcde"},
             "action": "deleted",
-            "comment": {
-                "node_id": "hijkl",
-                "pull_request_review_id": "1234566"
-            }
+            "comment": {"node_id": "hijkl", "pull_request_review_id": "1234566"},
         }
 
         webhook._handle_pull_request_review_comment(payload)
