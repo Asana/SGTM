f1e1fece011dc47bd18455835c0ec6307f3c333b
Greg Einfrank <greg.einfrank@gmail.com>
2020-08-28 16:51:02 -0700

    Use different URL REGEX

--- src/asana/helpers.py
+++ src/asana/helpers.py
@@ -8,7 +8,8 @@ from src.asana import client as asana_client
 from src.github import logic as github_logic
 from src.logger import logger
 
-URL_REGEX = '([^"\>\<]|^)(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
+# https://gist.github.com/gruber/8891611
+URL_REGEX = r"""(?i)([^"\>\<]|^)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))"""
 
 
 def task_url_from_task_id(task_id: str) -> str:
@@ -242,9 +243,9 @@ def convert_urls_to_links(text: str) -> str:
     Asana's API.
     """
 
-    def urlreplace(matchobj):
-        return '{}<A href="{}">{}</A>'.format(
-            matchobj.group(1), matchobj.group(2), matchobj.group(2)
+    def urlreplace(matchobj: Match[str]) -> str:
+        return matchobj.group(1) + _wrap_in_tag("A", attrs={"href": matchobj.group(2)})(
+            matchobj.group(2)
         )
 
     return re.sub(URL_REGEX, urlreplace, text)
