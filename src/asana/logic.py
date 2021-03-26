887364ec4d1d9063a2db626eeedb1ebf5297e049
Greg Einfrank <greg.einfrank@gmail.com>
2021-03-26 08:59:43 -0700

    test black failure again

--- src/asana/logic.py
+++ src/asana/logic.py
@@ -12,7 +12,9 @@ class AutocompleteLabel(Enum):
 def should_autocomplete_tasks_on_merge(pull_request: PullRequest) -> bool:
 
     return (
+
         SGTM_FEATURE__AUTOCOMPLETE_ENABLED
+
         and pull_request.merged()
         and pull_request_has_label(
             pull_request, AutocompleteLabel.COMPLETE_ON_MERGE.value
