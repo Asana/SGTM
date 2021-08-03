31b88cae42af2e66176d4c3b11b4e204297fb4c4
Owen Auch <owenauch@asana.com>
2021-08-03 16:57:57 -0700

    Add a Markdown parser to fix bugs with comment parsing (#100)
    * we basically did it
    
    * this is about as well as I can do
    
    * nits
    
    * nits
    
    * nits
    
    * fix pre tag issue
    
    * linter
    
    * final fixes

---
+++ src/markdown_parser.py
@@ -0,0 +1,43 @@
+import re
+from html import escape, unescape
+from typing import Match
+
+import mistune  # type: ignore
+
+# https://gist.github.com/gruber/8891611
+URL_REGEX = r"""(?i)([^"\>\<\/\.]|^)\b((?:https?:(/{1,3}))(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))"""
+
+
+class GithubToAsanaRenderer(mistune.HTMLRenderer):
+    def paragraph(self, text):
+        return text + "\n"
+
+    def block_quote(self, text):
+        return f"<em>> {escape(text, quote=False)}</em>"
+
+    def strikethrough(self, text):
+        return f"<s>{escape(text, quote=False)}</s>"
+
+    def heading(self, text, level):
+        return f"\n<b>{escape(text, quote=False)}</b>\n"
+
+    def text(self, text):
+        text = escape(text, quote=False)
+
+        def urlreplace(matchobj: Match[str]) -> str:
+            url = unescape(matchobj.group(2))
+            return matchobj.group(1) + f'<a href="{escape(url, quote=False)}">{url}</a>'
+
+        return re.sub(URL_REGEX, urlreplace, text)
+
+
+def convert_github_markdown_to_asana_xml(text: str) -> str:
+    markdown = mistune.create_markdown(
+        renderer=GithubToAsanaRenderer(escape=False), plugins=["strikethrough"],
+    )
+    return _strip_pre_tags(markdown(text))
+
+
+# the Asana API doesn't accept pre tags so we strip them
+def _strip_pre_tags(text: str) -> str:
+    return text.replace("<pre>", "").replace("</pre>", "")
---
+++ test/test_markdown_parser.py
@@ -0,0 +1,61 @@
+import unittest
+
+from src.markdown_parser import convert_github_markdown_to_asana_xml
+
+
+class TestConvertGithubMarkdownToAsanaXml(unittest.TestCase):
+    def test_basic_markdown(self):
+        md = """~~strike~~ **bold** _italic_ `code` [link](asana.com)"""
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(
+            xml,
+            '<s>strike</s> <strong>bold</strong> <em>italic</em> <code>code</code> <a href="asana.com">link</a>\n',
+        )
+
+    def test_ul_tag(self):
+        md = """* bullet one\n* bullet two"""
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(
+            xml, """<ul>\n<li>bullet one</li>\n<li>bullet two</li>\n</ul>\n""",
+        )
+
+    def test_ol_tag(self):
+        md = """1. bullet one\n2. bullet two"""
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(
+            xml, """<ol>\n<li>bullet one</li>\n<li>bullet two</li>\n</ol>\n""",
+        )
+
+    def test_paragraph(self):
+        md = "we don't wrap random text in p tags"
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(md + "\n", xml)
+
+    def test_block_quote(self):
+        md = "> block quote"
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(xml, "<em>> block quote\n</em>")
+
+    def test_auto_linking(self):
+        md = "https://asana.com/ [still works](www.test.com)"
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(
+            xml,
+            '<a href="https://asana.com/">https://asana.com/</a> <a href="www.test.com">still works</a>\n',
+        )
+
+    def test_converts_headings_to_bold(self):
+        md = "## heading"
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(xml, "\n<b>heading</b>\n")
+
+    def test_removes_pre_tags(self):
+        md = """```test```"""
+        xml = convert_github_markdown_to_asana_xml(md)
+        self.assertEqual(xml, "<code>test</code>\n")
+
+
+if __name__ == "__main__":
+    from unittest import main as run_tests
+
+    run_tests()
