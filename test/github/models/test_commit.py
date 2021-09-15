74e3187b946bf2b56f5812e3d98b000a849bea7a
Greg Einfrank <greg.einfrank@gmail.com>
2021-09-15 11:13:18 -0700

    black

--- test/github/models/test_commit.py
+++ test/github/models/test_commit.py
@@ -5,28 +5,22 @@ from src.github.models import Commit
 
 class TestCommit(BaseClass):
     def test_status_success(self):
-        raw_commit = {"commit": {
-            "status": {
-                "state": Commit.BUILD_SUCCESSFUL
-            }
-        }}
+        raw_commit = {"commit": {"status": {"state": Commit.BUILD_SUCCESSFUL}}}
         commit = Commit(raw_commit)
         self.assertEqual(commit.status(), Commit.BUILD_SUCCESSFUL)
 
     def test_status_none(self):
-        raw_commit = {"commit": {
-            "status": None
-        }}
+        raw_commit = {"commit": {"status": None}}
         commit = Commit(raw_commit)
         self.assertEqual(commit.status(), None)
 
     def test_status_falls_back_to_status_rollup(self):
-        raw_commit = {"commit": {
-            "status": None,
-            "statusCheckRollup": {
-                "state": Commit.BUILD_PENDING
+        raw_commit = {
+            "commit": {
+                "status": None,
+                "statusCheckRollup": {"state": Commit.BUILD_PENDING},
             }
-        }}
+        }
         commit = Commit(raw_commit)
         self.assertEqual(commit.status(), Commit.BUILD_PENDING)
 
