4a00ddda11e0872af579db1409dc8b5f58f50f71
Lance Rutkin <lancerutkin@asana.com>
2022-04-05 15:27:32 -0400

    Grammar fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -333,8 +333,8 @@ resource "aws_kms_key" "api_encryption_key" {
 }
 
 # This is a temporary fix for the breaking changes made to the "aws_s3_bucket" resource in v4.0 of the Terraform AWS Provider
-# This fix is from one of the issues raised regarding the breaking change https://github.com/hashicorp/terraform-provider-aws/issues/23125#issuecomment-1036412659.
-# Ongoing discussion regarding the breaking change has been moved to this issue https://github.com/hashicorp/terraform-provider-aws/issues/23106.
+# This fix is from one of the issues raised regarding the breaking change. https://github.com/hashicorp/terraform-provider-aws/issues/23125#issuecomment-1036412659
+# Ongoing discussion regarding the breaking change has been moved to this issue. https://github.com/hashicorp/terraform-provider-aws/issues/23106
 terraform {
   required_providers {
     aws = "~> 3.27"
