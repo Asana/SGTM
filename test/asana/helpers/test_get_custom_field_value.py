8967546147af1aa5683c0ffd1ea4f9b3a7b28ced
Greg Einfrank <gregeinfrank@asana.com>
2022-12-27 21:23:51 -0800

    fix tests

--- test/asana/helpers/test_get_custom_field_value.py
+++ test/asana/helpers/test_get_custom_field_value.py
@@ -1,7 +1,7 @@
 from src.asana import helpers as asana_helpers
 
 from test.impl.base_test_case_class import BaseClass
-from test.impl.builders.custom_field_builder import get_custom_field_settings_for_test
+from test.impl.builders.custom_field_builder import get_enum_custom_field_setting_for_test
 
 
 class TestGetCustomFieldValue(BaseClass):
@@ -13,7 +13,7 @@ class TestGetCustomFieldValue(BaseClass):
         cls.enabled_enum_option_name = "enabled"
         cls.disabled_enum_option_gid = "33333"
         cls.disabled_enum_option_name = "disabled"
-        cls.custom_field_data = get_custom_field_settings_for_test(
+        cls.custom_field_data = get_enum_custom_field_setting_for_test(
             custom_field_gid=cls.custom_field_gid,
             custom_field_name=cls.custom_field_name,
             enabled_enum_option_gid=cls.enabled_enum_option_gid,
@@ -25,49 +25,34 @@ class TestGetCustomFieldValue(BaseClass):
     def test_get_valid_people_custom_field_value(self):
         cf_name = "Author"
         expected_cf_value = "9999999999"
-        custom_field_data = [
-            {
-                "gid": "1243324",
-                "custom_field": {
+        custom_field_data = {
                     "name": cf_name,
                     "resource_type": "custom_field",
                     "resource_subtype": "people",
-                },
-                "resource_type": "custom_field_setting",
             }
-        ]
         cf_value = asana_helpers._get_custom_field_value(
-            cf_name,
-            expected_cf_value,
             custom_field_data,
+            expected_cf_value,
         )
         self.assertEqual(cf_value, expected_cf_value)
 
     def test_get_valid_custom_field_enum_option_id(self):
         enum_option_id = asana_helpers._get_custom_field_value(
-            self.custom_field_name,
-            self.enabled_enum_option_name,
             self.custom_field_data,
+            self.enabled_enum_option_name,
         )
         self.assertEqual(enum_option_id, self.enabled_enum_option_gid)
 
-    def test_invalid_custom_field(self):
-        enum_option_id = asana_helpers._get_custom_field_value(
-            "RandomField", self.enabled_enum_option_name, self.custom_field_data
-        )
-        self.assertIsNone(enum_option_id)
-
     def test_invalid_enum_option(self):
         enum_option_id = asana_helpers._get_custom_field_value(
-            self.custom_field_name, "RandomEnumOption", self.custom_field_data
+            self.custom_field_data, "RandomEnumOption"
         )
         self.assertIsNone(enum_option_id)
 
     def test_get_enum_option_id_for_disabled_option(self):
         enum_option_id = asana_helpers._get_custom_field_value(
-            self.custom_field_name,
-            self.disabled_enum_option_name,
             self.custom_field_data,
+            self.disabled_enum_option_name,
         )
         self.assertIsNone(enum_option_id)
 
--- test/impl/builders/custom_field_builder.py
+++ test/impl/builders/custom_field_builder.py
@@ -1,18 +1,15 @@
 from typing import List, Dict
 
 
-def get_custom_field_settings_for_test(
+def get_enum_custom_field_setting_for_test(
     custom_field_gid: str = "12345",
     custom_field_name: str = "test",
     enabled_enum_option_gid: str = "67890",
     enabled_enum_option_name: str = "enabled option",
     disabled_enum_option_gid: str = "67890",
     disabled_enum_option_name: str = "disabled option",
-) -> List[Dict]:
-    return [
-        {
-            "gid": "11111",
-            "custom_field": {
+) -> Dict:
+    return {
                 "gid": custom_field_gid,
                 "enum_options": [
                     {
@@ -34,18 +31,4 @@ def get_custom_field_settings_for_test(
                 "resource_subtype": "enum",
                 "resource_type": "custom_field",
                 "type": "enum",
-            },
-            "is_important": True,
-            "parent": {
-                "gid": "22222",
-                "name": "Test Project",
-                "resource_type": "project",
-            },
-            "project": {
-                "gid": "33333",
-                "name": "Test Project",
-                "resource_type": "project",
-            },
-            "resource_type": "custom_field_setting",
         }
-    ]
