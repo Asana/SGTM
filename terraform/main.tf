309edebaf3db6bbb783dd2df102e384f74f5adce
Vi Nguyen <vinguyen@asana.com>
2024-04-17 14:50:09 -0700

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -314,7 +314,7 @@ resource "aws_api_gateway_integration" "sgtm_sqs_integration" {
 
 resource "aws_api_gateway_integration_response" "sgtm_proxy_response_sqs" {
   depends_on  = [aws_api_gateway_integration.sgtm_sqs_integration]
-  rest_api_id = aws_api_gateway_rest_api.sgtm_rest_api_with_sqs.id
+  rest_api_id = aws_api_gateway_rest_api.sgtm_rest_api.id
   resource_id = aws_api_gateway_resource.sgtm_resource.id
   http_method = aws_api_gateway_method.sgtm_post.http_method
   status_code = aws_api_gateway_method_response.proxy.status_code
