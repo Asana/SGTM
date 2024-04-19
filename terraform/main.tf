eaec845acbb676b26a03f45f0a5e87127f76ccc8
Vi Nguyen <vinguyen@asana.com>
2024-04-19 13:37:50 -0700

    logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,7 @@ resource "aws_api_gateway_rest_api" "sgtm_rest_api" {
   }
 }
 
+
 ### DYNAMODB
 # ##DynamoDbSchema The DynamoDbSchema's source of truth is to be found here, in the sgtm/terraform/main.tf, except for
 # the sgtm_terraform_state_lock table, which has it's source of truth in scripts/setup.py
