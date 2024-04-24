dcc785373bfe655bade989a66e3c3a2b784b8d5b
Harshita Gupta <harshita-gupta@users.noreply.github.com>
2024-04-24 16:36:00 -0700

    commit created by maybePromptToGcam()

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -6,37 +6,38 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 echo "Executing create_pkg.sh..."
 
+echo "Source code path is set to $source_code_path"
 cd $path_cwd
-dir_name=lambda_dist_pkg/
-mkdir -p $dir_name
+dist_dir_name=lambda_dist_pkg/
 
-# Create and activate virtual environment...
-$runtime -m venv env_$function_name
-source $path_cwd/env_$function_name/bin/activate
+echo "Creating directory $dist_dir_name within $path_cwd..."
+mkdir -p $dist_dir_name
 
-# Installing python dependencies...
-FILE=$path_cwd/../requirements.txt
+# Create and activate virtual environment, and install python dependencies...
+# echo "Creating and activating virtual environment..."
+pipenv install
 
-if [ -f "$FILE" ]; then
-  echo "Installing dependencies..."
-  echo "From: requirements.txt file exists..."
-  pip3 install -r "$FILE"
-
-else
-  echo "Error: requirements.txt does not exist!"
-fi
-
-# Deactivate virtual environment...
-deactivate
+# Activate virtual environment. This is the equivalent of prefixing all the below commands with
+# `pipenv run`
+pipenv shell
 
 # Create deployment package...
 echo "Creating deployment package..."
-cd env_$function_name/lib/$runtime/site-packages/
-cp -r . $path_cwd/$dir_name
-cp -r $path_cwd/$source_code_path $path_cwd/$dir_name
 
-# Removing virtual environment folder...
-echo "Removing virtual environment folder..."
-rm -rf $path_cwd/env_$function_name
+# Install dependencies to the deployment package directory
+pip install -r <(pipenv lock -r) --target $dist_dir_name
+pip freeze | grep cryptography
+if [ $? -ne 0 ]; then
+  echo "cryptography isn't installed; will skip retrieving linux version of cryptography"
+else
+  echo "cryptography is installed; will retrieve linux version of cryptography"
+  # Install cryptography package for manylinux2014_x86_64 to the package directory
+  pip install --platform manylinux2014_x86_64 --implementation cp --only-binary=:all: --upgrade --target $dist_dir_name cryptography
+fi
 
+# Copy source code to the deployment package directory
+cp -R "$path_cwd"/"$source_code_path" "$path_cwd"/$dist_dir_name
+
+# Deactivate virtual environment...
+deactivate
 echo "Finished script execution!"
