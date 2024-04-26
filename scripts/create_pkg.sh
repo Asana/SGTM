0bdd0173c830690b8fdde654e52f3143a5f2f56f
Harshita Gupta <harshita-gupta@users.noreply.github.com>
2024-04-25 20:34:09 -0700

    fix some of the errors

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -8,7 +8,9 @@ echo "Executing create_pkg.sh..."
 
 echo "Source code path is set to $source_code_path"
 cd $path_cwd
+# PIPENV_PIPFILE=$path_cwd/Pipfile
 dist_dir_name=lambda_dist_pkg/
+echo "$PIPENV_PIPFILE"
 
 echo "Creating directory $dist_dir_name within $path_cwd..."
 mkdir -p $dist_dir_name
@@ -17,22 +19,18 @@ mkdir -p $dist_dir_name
 # echo "Creating and activating virtual environment..."
 pipenv install
 
-# Activate virtual environment. This is the equivalent of prefixing all the below commands with
-# `pipenv run`
-pipenv shell
-
 # Create deployment package...
 echo "Creating deployment package..."
 
 # Install dependencies to the deployment package directory
-pip install -r <(pipenv lock -r) --target $dist_dir_name
-pip freeze | grep cryptography
+pipenv run pip install -r <(pipenv requirements) --target $dist_dir_name
+pipenv run pip freeze | grep cryptography
 if [ $? -ne 0 ]; then
   echo "cryptography isn't installed; will skip retrieving linux version of cryptography"
 else
   echo "cryptography is installed; will retrieve linux version of cryptography"
   # Install cryptography package for manylinux2014_x86_64 to the package directory
-  pip install --platform manylinux2014_x86_64 --implementation cp --only-binary=:all: --upgrade --target $dist_dir_name cryptography
+  pipenv run pip install --platform manylinux2014_x86_64 --implementation cp --only-binary=:all: --upgrade --target $dist_dir_name cryptography
 fi
 
 # Copy source code to the deployment package directory
