fd6d29e42324882c6a2774e21d23833ab0669ca9
Vi Nguyen <vinguyen@asana.com>
2024-04-30 11:30:24 -0700

    check if none

--- test/impl/builders/check_run_builder.py
+++ test/impl/builders/check_run_builder.py
@@ -17,7 +17,7 @@ class CheckRunBuilder(BuilderBaseClass):
     def completed_at(
         self, completed_at: Union[str, datetime, None]
     ) -> Union["CheckRunBuilder", CheckRun]:
-        self.raw_check_run["completedAt"] = transform_datetime(completed_at)
+        self.raw_check_run["completedAt"] = transform_datetime(completed_at) if completed_at else None
         return self
 
     def build(self) -> CheckRun:
