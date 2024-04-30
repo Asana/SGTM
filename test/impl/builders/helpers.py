f95b48287523452a2ab36b6ce61c92b429688d19
Vi Nguyen <vinguyen@asana.com>
2024-04-30 13:33:03 -0700

    fix

--- test/impl/builders/helpers.py
+++ test/impl/builders/helpers.py
@@ -1,10 +1,10 @@
 from uuid import uuid4
 from datetime import datetime
-from typing import Union, Optional
+from typing import Union
 from src.utils import create_date_string
 
 
-def transform_datetime(input_datetime: Union[str, datetime, None]) -> Optional[str]:
+def transform_datetime(input_datetime: Union[str, datetime]) -> str:
     if isinstance(input_datetime, datetime):
         return create_date_string(input_datetime)
     return input_datetime
