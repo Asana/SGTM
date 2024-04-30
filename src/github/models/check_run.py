a753ab7ce1eaa60a2eac16d9c525cb489db3be1e
Vi Nguyen <vinguyen@asana.com>
2024-04-29 18:01:28 -0700

    fix type

--- src/github/models/check_run.py
+++ src/github/models/check_run.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 from src.utils import parse_date_string
 
 import copy
@@ -9,7 +9,7 @@ class CheckRun(object):
     def __init__(self, raw_check_run: Dict[str, Any]):
         self._raw = copy.deepcopy(raw_check_run)
 
-    def completed_at(self) -> datetime:
+    def completed_at(self) -> Optional[datetime]:
         completion_time = self._raw.get("completedAt")
         return parse_date_string(completion_time) if completion_time else None
 
