d1850cc7f3ea304f3dbfce054d3891ac9ed954e1
Vi Nguyen <vinguyen@asana.com>
2024-05-13 16:24:12 -0700

    error

--- src/github/controller.py
+++ src/github/controller.py
@@ -17,7 +17,7 @@ def upsert_pull_request(pull_request: PullRequest):
         task_id = asana_controller.create_task(pull_request.repository_id())
         if task_id is None:
             # TODO: Handle this case
-            logger.info(f"Failed to create task for pull request {pull_request_id}")
+            logger.error(f"Failed to create task for pull request {pull_request_id}")
             return
 
         logger.info(f"Task created for pull request {pull_request_id}: {task_id}")
