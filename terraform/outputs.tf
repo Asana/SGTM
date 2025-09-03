6e8bb157cd484f6b53fce92d8657dd59cac5c0ac
Susan Li <susanli@asana.com>
2025-09-03 09:39:45 -0700

    add prod and staging url outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "api_gateway_stage_invoke_url" {
+output "api_gateway_prod_invoke_url" {
   value = module.sgtm-prod.api_gateway_stage_invoke_url
+}
+
+output "api_gateway_staging_invoke_url" {
+  value = module.sgtm-staging.api_gateway_stage_invoke_url
 }
